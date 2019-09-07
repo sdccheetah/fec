fcf69344f16c643fb6c7c52d1423057d2972a1ba
Dan Sha <danyun.sha@gmail.com>
2019-09-07 11:58:27 -0400

    Start on static product page URLs

--- server/server.js
+++ server/server.js
@@ -5,5 +5,7 @@ const port = 3000;
 app.use(express.static('./client/dist'));
 
 app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/test', (req, res) => res.send('./client/dist'));
+app.get('/products/:id', (req, res) => res.send(req.params.id));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
