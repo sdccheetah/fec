ff461587ce8b7c0ae1187f684fad4d1287266435
psfonseka <psfonseka@gmail.com>
2019-09-11 11:47:28 -0400

    fixed multiple image bug using a random string generator for the ids

--- client/src/components/reviews/ImageComponent.js
+++ client/src/components/reviews/ImageComponent.js
@@ -3,14 +3,18 @@ import React from "react";
 class ImageComponent extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { isOpen: false };
+        this.state = { 
+            isOpen: false,
+            id: Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
+
+         };
 
         this.handleShowModal = this.handleShowModal.bind(this);
     }
 
     handleShowModal(event) {
         this.setState({ isOpen: !this.state.isOpen }, () => {
-            var modal = document.getElementById(this.props.id);
+            var modal = document.getElementById(this.state.id);
             if (this.state.isOpen) {
                 modal.style.display = "block"
             } else {
@@ -28,7 +32,7 @@ class ImageComponent extends React.Component {
                 onClick={this.handleShowModal}
                 alt="no image"
             />
-            <div id={this.props.id} className="review-modal" onClick={this.handleShowModal}>
+            <div id={this.state.id} className="review-modal" onClick={this.handleShowModal}>
             
                 <div className="review-modal-content">
                     <img src={this.props.source}></img>
