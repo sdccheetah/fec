1091dee78651f192935cf2ad35660e4ecb8f6974
Kellyanne Revenaugh <kellyrevenaugh@gmail.com>
2019-09-13 14:48:48 -0400

    update buttons

--- client/src/components/qa/Answers.js
+++ client/src/components/qa/Answers.js
@@ -86,8 +86,6 @@ class Answers extends React.Component {
             <Button
               className='qa-put-option'
               variant='text'
-              text-size='10px'
-              className='upper-button'
               onClick={() => {
                 this.loadMore();
               }}>
@@ -96,7 +94,7 @@ class Answers extends React.Component {
           ) : this.state.load > 2 ? (
             <Button
               variant='text'
-              className='loadmore'
+              className='qa-put-option'
               onClick={() => {
                 this.resetAnswers();
               }}>
